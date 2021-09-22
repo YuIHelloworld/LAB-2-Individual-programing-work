eaa456012faa8571baea9792108a233f896ca4f2
YuIHelloworld <58470560+YuIHelloworld@users.noreply.github.com>
2021-09-22 19:59:34 +0800

    Add files via upload

---
+++ LAB2/code.cpp
@@ -0,0 +1,16 @@
+	#include <stdio.h>  
+	int main(){  
+	    int i=1;  
+	    double j=0;  
+	    long f;  
+	    switch(i){  
+	        case 0:  
+	            break;  
+	        case 1:  
+	            break;  
+	        case 2:  
+	            break;  
+	        default:  
+	            break; 
+		   }  
+
